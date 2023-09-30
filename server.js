72e3197478e8aa7e99334656679926afb4aa3a90
Daniyal.malik <Daniyal.malik@int.sybrid.com>
2023-09-30 19:53:10 +0500

    chaange env files

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const express = require('express'),
   morgan = require('morgan'),
   error = require('./middleware/error');
 
-dotenv.config({ path: 'config/config.env' });
+dotenv.config({ path: 'config/.env' });
 
 app.use(cors());
 app.use(express.json());
@@ -19,6 +19,31 @@ app.use(morgan('dev'));
 app.use('/api/user', userRoutes);
 app.use('/api/upload', uploadRoutes);
 app.use(error);
+
+app.get('/', async (req, res, next) => {
+  try {
+    let promise = new Promise((resolve, reject) =>
+      setTimeout(() => {
+        resolve('resolved');
+        console.log('here!');
+      }, 10000),
+    );
+
+    promise;
+
+    res.json({
+      success: true,
+      message: 'Success',
+    });
+  } catch (error) {
+    console.log(error);
+
+    res.json({
+      success: false,
+      message: 'An error occurred!',
+    });
+  }
+});
 
 const PORT = process.env.PORT || 5000,
   ENVIRONMENT = process.env.NODE_ENV,
